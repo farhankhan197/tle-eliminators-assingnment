ee6e5e20042d96222e1600ae6172d7c6ff33ab52
Ayush Sharma <ayush.sharma74892@gmail.com>
2025-10-16 15:17:54 +0530

    fix api

--- backend/services/leetcode.js
+++ backend/services/leetcode.js
@@ -12,6 +12,43 @@ export async function getLeetCodeContests() {
     }
   `;
 
+  const queryPastContest =`
+ query pastContests($pageNo:Int,$numPerPage:Int) {
+pastContests(pageNo: $pageNo, numPerPage:$numPerPage){
+pageNum
+currentPage
+totalNum
+numPerPage
+data { 
+  title 
+  titleSlug
+  startTime 
+  originStartTime 
+  cardImg
+  sponsors{
+    name 
+    lightLogo
+    darkLogo
+    }
+  } 
+}
+}`;
+console.log("Query Start")
+  const responsePast = await axios.post(
+    "https://leetcode.com/graphql/",
+    {query: queryPastContest,
+      variables: {pageNo:1, numPerPage: 10}
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Referer: "https://leetcode.com",
+        "User-Agent": "Mozilla/5.0",
+      },
+    }
+  );
+  console.log("Query Ends")
+  console.log(responsePast.data);
   try {
     const response = await axios.post(
       "https://leetcode.com/graphql/",
@@ -19,16 +56,16 @@ export async function getLeetCodeContests() {
       {
         headers: {
           "Content-Type": "application/json",
-          "Referer": "https://leetcode.com",
-          "User-Agent": "Mozilla/5.0"
+          Referer: "https://leetcode.com",
+          "User-Agent": "Mozilla/5.0",
         },
       }
     );
-    console.log(response.data);
+    // console.log(response.data);
 
     const contests = response.data.data.upcomingContests || [];
     console.log("✅ Fetched LeetCode contests:", contests);
-    return contests.map(c => ({
+    return contests.map((c) => ({
       platform: "LeetCode",
       name: c.title,
       url: `https://leetcode.com/contest/${c.titleSlug}`,
@@ -39,5 +76,4 @@ export async function getLeetCodeContests() {
   } catch (err) {
     console.error("❌ LeetCode API Error:", err.message);
     return [];
-  }
-}
+  }}
